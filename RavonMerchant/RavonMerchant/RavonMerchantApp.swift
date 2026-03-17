12193001d4538611c82ceabc3861c6f07cb3a419
Muhammadjon Marufov <mmarufov@dons.usfca.edu>
2026-03-17 15:25:44 -0700

    Track merchant app files directly

---
+++ RavonMerchant/RavonMerchant/RavonMerchantApp.swift
@@ -0,0 +1,17 @@
+//
+//  RavonMerchantApp.swift
+//  RavonMerchant
+//
+//  Created by Muhammad Marufov on 3/17/26.
+//
+
+import SwiftUI
+
+@main
+struct RavonMerchantApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
